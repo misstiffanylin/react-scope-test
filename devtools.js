dd745b8c80766e30c32ea62a9e60d2ede3540b59
Tiffany Lin <tiffanylin@Andrew-Chan.local>
2018-01-13 18:24:37 -0800

    styling

--- devtools.js
+++ devtools.js
@@ -24,12 +24,53 @@ function sendMessage() {
     else {
       console.log("data is here")
       storage = msg;
-      console.log(storage)
-      // let example = storage.data[1].children[3].name;
-      // var node = document.createElement('h2');
+      storage = storage.data.head.value.currentState[1].children[3]
+      const data = [];
+        while(storage) { // while storage isn't null, continue
+          // if (storage.name) { // add to data obj if there is a name property
+            console.log('looping')
+            if(storage.children.length > 1) {
+              for (keys in storage.children.length) {
+                data.push(getChildren(keys))
+              }
+            } else {
+              data.push({
+                name : storage.name,
+                props : storage.props,
+                state : storage.states,
+              })
+            }
+          // }
+          storage = storage.children[Object.keys(storage.children)[0]]
+        }
+        // console.log(storage.data.head.value.currentState[1].children[3])
+      
+      function getChildren(child) {
+        while(child) { // while storag isn't null, continue
+        // if (child.name) { // add to data obj if there is a name property
+          console.log('looping')
+          if(child.children.length > 1) {
+            data.push()
+          }
+          data.push({
+            name : child.name,
+            props : child.props,
+            state : child.states,
+          })
+        // }
+        child = child.children[Object.keys(storage.children)[0]]
+      }
+      }
+
+      console.log(data)
+      // let example = 'hello';
+      // var node = document.createElement('h4');
       // var textnode = document.createTextNode(example);
       // node.appendChild(textnode);
       // document.getElementById('app').appendChild(node);
+
     }
   })
 };
+
+
